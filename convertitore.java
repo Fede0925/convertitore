78180aca65c80cbbde5d0e58bde76da24ff2563e
Federico <federico.mazzetti@iisgubbio.edu.it>
2024-03-26 17:21:42 +0100

    add conversione da base A a base B

--- convertitore.java
+++ convertitore.java
@@ -12,7 +12,8 @@ import javafx.stage.Stage;
 
 public class convertitore extends Application{
 	Button calcola = new Button("converti");
-	TextField tBase = new TextField();
+	TextField tBaseA = new TextField();
+	TextField tBaseB = new TextField();
 	TextField tNumero = new TextField();
 	Label lRis = new Label();
 	
@@ -25,11 +26,14 @@ public class convertitore extends Application{
 		griglia.setHgap(10);
 		griglia.setVgap(10);
 		griglia.add(calcola, 0, 0);
-		griglia.add(tBase, 0, 1);
-		griglia.add(tNumero, 1, 1);
-		griglia.add(lRis, 0, 2);
+		griglia.add(tBaseA, 0, 1);
+		griglia.add(tBaseB, 1, 1);
+		griglia.add(tNumero, 0, 2, 2, 1);
+		griglia.add(lRis, 0, 3);
 		
-		tBase.setPromptText("Base di arrivo");
+		
+		tBaseA.setPromptText("Base di partenza");
+		tBaseB.setPromptText("Base di arrivo");
 		tNumero.setPromptText("Numero da convertire");
 		
 		calcola.setOnAction(e->calcola());
@@ -40,62 +44,254 @@ public class convertitore extends Application{
 	}
 
 	private void calcola() {
-		int base = Integer.parseInt(tBase.getText());
-		int numero = Integer.parseInt(tNumero.getText());
+		int baseA = Integer.parseInt(tBaseA.getText());
+		int baseB = Integer.parseInt(tBaseB.getText());
+		int numero = 0;
+		String sNumero;
+		if(baseA!=16) {
+			numero = Integer.parseInt(tNumero.getText());
+			sNumero = numero + "";
+		}else {
+			sNumero = tNumero.getText();
+		}
 		int posto;
-		String convertito="";
+		String vsNum[];
+		int vNum[];
+		String convertito = "";
+		int iConv = 0;
 		
-		if(base!=16) {
+		if(baseB!=16 && baseA == 10) {
 			while (numero!=0) {
-				posto=numero-((numero/base)*base);
+				posto=numero-((numero/baseB)*baseB);
 				convertito+=posto+" ";
-				numero=numero/base;
+				numero=numero/baseB;
+			}
+			String inverti[] = convertito.split(" ");
+			convertito="";
+			
+			for(int i=inverti.length-1;i>=0;i--) {
+				convertito+=inverti[i];
 			}
 		}
 		
-		if(base==16) {
+		if(baseA==10 && baseB==16) {
 			while (numero!=0) {
-				posto=numero-((numero/base)*base);
+				posto=numero-((numero/baseB)* baseB);
 				
 				if(posto<10) {
 					convertito+=posto+" ";
-					numero=numero/base;
+					numero=numero/baseB;
 				}
 				
 				if(posto>=10) {
 					if(posto==10) {
 						convertito+="A"+" ";
-						numero=numero/base;
+						numero=numero/baseB;
 					}
 					if(posto==11) {
 						convertito+="B"+" ";
-						numero=numero/base;
+						numero=numero/baseB;
 					}
 					if(posto==12) {
 						convertito+="C"+" ";
-						numero=numero/base;
+						numero=numero/baseB;
 					}
 					if(posto==13) {
 						convertito+="D"+" ";
-						numero=numero/base;
+						numero=numero/baseB;
 					}
 					if(posto==14) {
 						convertito+="E"+" ";
-						numero=numero/base;
+						numero=numero/baseB;
 					}
 					if(posto==15) {
 						convertito+="F"+" ";
-						numero=numero/base;
+						numero=numero/baseB;
 					}
 				}
 			}
+			String inverti[] = convertito.split(" ");
+			convertito="";
+			
+			for(int i=inverti.length-1;i>=0;i--) {
+				convertito+=inverti[i];
+			}
+		}
+			
+		if(baseB==10 && baseA!=16) {
+			vsNum = sNumero.split("");
+			vNum = new int[vsNum.length];
+			
+			for(int i=0;i<vsNum.length;i++) {
+				vNum[i] = Integer.parseInt(vsNum[i]);
+			}
+			
+			for(int i=0;i<vNum.length;i++) {
+				iConv += (int) (vNum[i] * Math.pow(baseA, vNum.length-(i+1)));
+			}
+			convertito+=iConv;
 		}
 		
-		String inverti[] = convertito.split(" ");
-		convertito="";
+		if(baseB==10 && baseA==16) {
+			vsNum = sNumero.split("");
+			vNum = new int[vsNum.length];
+			
+			for(int i=0;i<vsNum.length;i++) {
+				if(vsNum[i].equals("A") || vsNum[i].equals("a")) {
+					vsNum[i]="10";
+				}
+				if(vsNum[i].equals("B") || vsNum[i].equals("b")) {
+					vsNum[i]="11";
+				}
+				if(vsNum[i].equals("C") || vsNum[i].equals("c")) {
+					vsNum[i]="12";
+				}
+				if(vsNum[i].equals("D") || vsNum[i].equals("d")) {
+					vsNum[i]="13";
+				}
+				if(vsNum[i].equals("E") || vsNum[i].equals("e")) {
+					vsNum[i]="14";
+				}
+				if(vsNum[i].equals("F") || vsNum[i].equals("f")) {
+					vsNum[i]="15";
+				}
+			}
+			for(int j=0;j<vsNum.length;j++) {
+				vNum[j] = Integer.parseInt(vsNum[j]);
+				}
+			
+			for(int i=0;i<vNum.length;i++) {
+				iConv += (int) (vNum[i] * Math.pow(baseA, vNum.length-(i+1)));
+			}
+			convertito+=iConv;
+		}
 		
-		for(int i=inverti.length-1;i>=0;i--) {
-			convertito+=inverti[i];
+		if(baseA!=10 && baseB!=10 && baseA!=16 && baseB!=16) {
+			vsNum = sNumero.split("");
+			vNum = new int[vsNum.length];
+			
+			for(int i=0;i<vsNum.length;i++) {
+				vNum[i] = Integer.parseInt(vsNum[i]);
+			}
+			
+			for(int i=0;i<vNum.length;i++) {
+				iConv += (int) (vNum[i] * Math.pow(baseA, vNum.length-(i+1)));
+			}
+			
+			numero=iConv;
+			while (numero!=0) {
+				posto=numero-((numero/baseB)*baseB);
+				convertito+=posto+" ";
+				numero=numero/baseB;
+			}
+			String inverti[] = convertito.split(" ");
+			convertito="";
+			
+			for(int i=inverti.length-1;i>=0;i--) {
+				convertito+=inverti[i];
+			}
+		}
+		
+		if(baseA==16 && baseB!=16) {
+			vsNum = sNumero.split("");
+			vNum = new int[vsNum.length];
+			
+			for(int i=0;i<vsNum.length;i++) {
+				if(vsNum[i].equals("A") || vsNum[i].equals("a")) {
+					vsNum[i]="10";
+				}
+				if(vsNum[i].equals("B") || vsNum[i].equals("b")) {
+					vsNum[i]="11";
+				}
+				if(vsNum[i].equals("C") || vsNum[i].equals("c")) {
+					vsNum[i]="12";
+				}
+				if(vsNum[i].equals("D") || vsNum[i].equals("d")) {
+					vsNum[i]="13";
+				}
+				if(vsNum[i].equals("E") || vsNum[i].equals("e")) {
+					vsNum[i]="14";
+				}
+				if(vsNum[i].equals("F") || vsNum[i].equals("f")) {
+					vsNum[i]="15";
+				}
+			}
+			for(int j=0;j<vsNum.length;j++) {
+				vNum[j] = Integer.parseInt(vsNum[j]);
+				}
+			
+			for(int i=0;i<vNum.length;i++) {
+				iConv += (int) (vNum[i] * Math.pow(baseA, vNum.length-(i+1)));
+			}
+			
+			numero=iConv;
+			while (numero!=0) {
+				posto=numero-((numero/baseB)*baseB);
+				convertito+=posto+" ";
+				numero=numero/baseB;
+			}
+			String inverti[] = convertito.split(" ");
+			convertito="";
+			
+			for(int i=inverti.length-1;i>=0;i--) {
+				convertito+=inverti[i];
+			}
+		}
+		
+		if(baseA!=16 && baseB==16) {
+			vsNum = sNumero.split("");
+			vNum = new int[vsNum.length];
+			
+			for(int i=0;i<vsNum.length;i++) {
+				vNum[i] = Integer.parseInt(vsNum[i]);
+			}
+			
+			for(int i=0;i<vNum.length;i++) {
+				iConv += (int) (vNum[i] * Math.pow(baseA, vNum.length-(i+1)));
+			}
+			
+			numero=iConv;
+			while (numero!=0) {
+				posto=numero-((numero/baseB)* baseB);
+				
+				if(posto<10) {
+					convertito+=posto+" ";
+					numero=numero/baseB;
+				}
+				
+				if(posto>=10) {
+					if(posto==10) {
+						convertito+="A"+" ";
+						numero=numero/baseB;
+					}
+					if(posto==11) {
+						convertito+="B"+" ";
+						numero=numero/baseB;
+					}
+					if(posto==12) {
+						convertito+="C"+" ";
+						numero=numero/baseB;
+					}
+					if(posto==13) {
+						convertito+="D"+" ";
+						numero=numero/baseB;
+					}
+					if(posto==14) {
+						convertito+="E"+" ";
+						numero=numero/baseB;
+					}
+					if(posto==15) {
+						convertito+="F"+" ";
+						numero=numero/baseB;
+					}
+				}
+			}
+			String inverti[] = convertito.split(" ");
+			convertito="";
+			
+			for(int i=inverti.length-1;i>=0;i--) {
+				convertito+=inverti[i];
+			}
 		}
 		
 		lRis.setText(convertito);
